d3d30e3782439acf30f7b9135da13b4027a0b6db
KMx404 <mohamedelfaleh5@gmail.com>
2021-07-05 01:44:15 +0100

    :bug: Modifying the styles structure in the css file

---
+++ .next/static/iMS9_-4mZqBH-91FyLexN/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/da3e6d69195c89a2291b.css","static/chunks/pages/index-84d70f83a7e6f605664c.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
